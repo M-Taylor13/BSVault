daad993dd3b463ded77aa410721fb78f7af6b14a
M-Taylor13 <madison.g.taylor@gmail.com>
2025-12-06 13:19:28 +1100

    Removed build and libsodium directories

--- build/CMakeFiles/BSVault.dir/BSVault_autogen/UYX5XTB5RZ/qrc_BSlogo.cpp.obj.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/BSVault.dir/BSVault_autogen/UYX5XTB5RZ/qrc_BSlogo.cpp.obj: \
- C:\Users\madis\OneDrive\Coding\BSVault\BSVault\build\BSVault_autogen\UYX5XTB5RZ\qrc_BSlogo.cpp
--- build/Desktop_Qt_6_10_0_MinGW_64_bit-Release1/qtcsettings.cmake
+++
@@ -1,3 +0,0 @@
-# This file is managed by Qt Creator, do not edit!
-
-set("CMAKE_PREFIX_PATH" "C:/Qt/6.10.0/mingw_64;<UNSET>" CACHE "PATH" "" FORCE)
